68199f659055a5081074524fb3f35068eefca38d
misterO <mistero4321@gmail.com>
2025-06-11 11:41:19 +0200

    feat : add Error components

---
+++ src/components/Error.js
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom"
+import "../styles/sass/Error.scss"
+
+export default function Error (){
+    return(
+        <main>
+            <span className="ErrorStatus">404</span>
+            <p>Oups, la page que vous demandez n'existe pas</p>
+            <Link>Retourner sur la page d'acceuil</Link>
+        </main>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './styles/sass/index.scss'
 import Header from './components/Header';
 import Footer from './components/Footer';
+import Error from './components/Error';
+// import Home from './components/Home';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -12,6 +14,10 @@ root.render(
   <React.StrictMode>
     <Router>
       <Header />
+      <Routes>
+        {/* <Route path='/' element={<Home />}/> */}
+        <Route path='*' element={<Error />}/>
+      </Routes>
       <Footer />
     </Router>
   </React.StrictMode>
