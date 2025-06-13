634cae5fa7bb5d0feb21afcfe227422a5a962563
misterO <mistero4321@gmail.com>
2025-06-13 16:30:17 +0200

    ci : HousingSheet component :  add number pages on the picture

--- src/components/HousingSheet.js
+++ src/components/HousingSheet.js
@@ -12,6 +12,11 @@ export default function HousingSheet(){
                 <svg className='arrow arrowForward' viewBox="0 0 48 81" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M0.959961 72.9877L8.03996 80.0677L47.64 40.4677L8.03996 0.867676L0.959961 7.94768L33.48 40.4677L0.959961 72.9877Z" fill="white"/>
                 </svg>
+                <span className='pages'>
+                    <span className='current-pages'>1</span>
+                    /
+                    <span className='total-pages'>4</span>
+                </span>
                 <source media='(max-width:768px)' srcSet={backgroundSmall} />
                 <img className='active' src={ background } alt='housing' />
             </picture>
