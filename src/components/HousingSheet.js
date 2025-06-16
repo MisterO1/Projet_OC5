1e164b10c3c0a34dcf364d0d576d0c467a1dc651
misterO <mistero4321@gmail.com>
2025-06-16 12:37:24 +0200

    feat : add "details"(description and equipments)

--- src/components/HousingSheet.js
+++ src/components/HousingSheet.js
@@ -58,11 +58,15 @@ export default function HousingSheet(){
                     </div>
                 </div>
             </section>
-            <section className='Details'>
+            <section className='details'>
                 <div className='description dropdown'>
                     <div className='menu'>
                         <span>Description</span>
-                        <button className='dropdown-btn'></button>
+                        <button className='dropdown-btn'>
+                            <svg viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M14.7897 10.7897C15.4591 10.1202 16.5462 10.1202 17.2157 10.7897L27.4979 21.0719C28.1674 21.7414 28.1674 22.8285 27.4979 23.4979C26.8285 24.1673 25.7414 24.1673 25.072 23.4979L16 14.4259L6.92804 23.4926C6.25862 24.162 5.17148 24.162 4.50206 23.4926C3.83265 22.8231 3.83265 21.736 4.50206 21.0666L14.7843 10.7843L14.7897 10.7897Z" />
+                            </svg>
+                        </button>
                     </div>
                     <div className='dropdown-content'>
                         Votre maison loin de chez vous. Que vous veniez de l'autre bout du monde, ou juste de quelques stations de RER, vous vous sentirez chez vous dans notre appartement.
@@ -71,7 +75,11 @@ export default function HousingSheet(){
                 <div className='equipment dropdown'>
                     <div className='menu'>
                         <span>Equipement</span>
-                        <button className='dropdown-btn'></button>
+                        <button className='dropdown-btn'>
+                            <svg viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M14.7897 10.7897C15.4591 10.1202 16.5462 10.1202 17.2157 10.7897L27.4979 21.0719C28.1674 21.7414 28.1674 22.8285 27.4979 23.4979C26.8285 24.1673 25.7414 24.1673 25.072 23.4979L16 14.4259L6.92804 23.4926C6.25862 24.162 5.17148 24.162 4.50206 23.4926C3.83265 22.8231 3.83265 21.736 4.50206 21.0666L14.7843 10.7843L14.7897 10.7897Z" />
+                            </svg>
+                        </button>
                     </div>
                     <div className='dropdown-content'>
                         <ul>
